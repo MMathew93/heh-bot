a5c15df2af85d71cc04ee9de40a5be54c12e5223
lyndexer <mckinney.m93@gmail.com>
2020-11-22 15:52:24 -0800

    fixed linter rules, fixed user cache issue

--- app.js
+++ app.js
@@ -11,12 +11,33 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 const User = require('./user');
 
 function degen(score) {
-	return score < 5 ? 'Weeny Boi'
-		: score < 15 ? 'Beany Boi'
-			: score < 25 ? 'Hakeem Hero'
-				: score < 50 ? 'Dillonstigator Expert'
-					: score < 100 ? 'GOT DAMN BOI'
-						: score < 150 ? 'God of Degenerates' : 'UwU';
+	let rank = '';
+	switch(true) {
+	case score < 5:
+		rank = 'Weeny Boi';
+		break;
+	case score < 15:
+		rank = 'Beany Boi';
+		break;
+	case score < 25:
+		rank = 'Hakeem Hero';
+		break;
+	case score < 50:
+		rank = 'Dillonstigator Expert';
+		break;
+	case score < 100:
+		rank = 'GOT DAMN BOI';
+		break;
+	case score < 150:
+		rank = 'Glhehzeey Gobbler';
+		break;
+	case score < 200:
+		rank = 'God of Degenerates';
+		break;
+	default:
+		rank = 'UwU';
+	}
+	return rank;
 }
 
 client.on('message', message => {
@@ -46,8 +67,7 @@ client.on('message', message => {
 						user.save();
 					}
 				});
-		}
-		catch(err) {
+		}catch(err) {
 			console.error(err);
 		}
 	}
@@ -59,24 +79,22 @@ client.on('message', message => {
 	if(message.content === `${prefix}hiscore`) {
 		try {
 			const result = db.collection('users').find().sort({ score: -1 }).limit(5);
-			result.toArray(function(err, user) {
-				const userList = user.map(function(x) {
-					const username = client.users.cache.get(x.userId);
-					return [username ? username.tag : 'Undefined', x.score];
-				});
+			result.toArray(async function(err, user) {
+				const userList = await Promise.all(user.map(async function(x) {
+					const listOfUsers = await client.users.fetch(x.userId);
+					return[listOfUsers ? listOfUsers.username : x.userId, x.score];
+				}));
 				let leaderboard = '**Leaderboard** \n';
 				for(let i = 0; i < userList.length; i++) {
 					if(i == 0) {
-						leaderboard += `${i + 1} - ${userList[i][0]} [${userList[i][1]}] points \n`;
-					}
-					else {
-						leaderboard += `${i + 1} - ${userList[i][0]} [${userList[i][1]}]points \n`;
+						leaderboard += `${i + 1} - ${userList[i][0]} [${ userList[i][1]}] points \n`;
+					}else {
+						leaderboard += `${i + 1} - ${userList[i][0]} [${ userList[i][1]}] points \n`;
 					}
 				}
 				message.channel.send(`${leaderboard}`);
 			});
-		}
-		catch(err) {
+		}catch(err) {
 			console.error(err);
 		}
 	}
@@ -92,13 +110,11 @@ client.on('message', message => {
 			}, function(err, user) {
 				if(!user) {
 					message.channel.send('Nothing personal kid, but you don\'t have a score to share!');
-				}
-				else {
+				}else {
 					message.channel.send(`${message.author.username}\nTotal: ${user.score}\nDegen Level: ${degen(user.score)}`);
 				}
 			});
-		}
-		catch(err) {
+		}catch(err) {
 			console.error(err);
 		}
 	}
@@ -122,13 +138,11 @@ client.on('message', message => {
 				if(err) { console.error(err);}
 				if(!user) {
 					message.channel.send('That user has no score!');
-				}
-				else {
+				}else {
 					message.channel.send(`${taggedUser.username}\nTotal: ${user.score}\nDegen Level: ${degen(user.score)}`);
 				}
 			});
-		}
-		catch(err) {
+		}catch(err) {
 			console.error(err);
 		}
 	}
@@ -141,8 +155,7 @@ client.on('message', message => {
 	if(message.content) {
 		if(easterEgg <= 0.01) {
 			message.channel.send('"Heh isn\'t something you throw out lightly. It is a cold dark phrase that destroys the humanity of a child\'s wonder. No one knows why heh was necessary, but we do know that your opinions do not matter. They will not matter and in the game of life, you have lost."');
-		}
-		else if(easterEgg <= 0.05) {
+		}else if(easterEgg <= 0.05) {
 			message.channel.send('heh');
 		}
 	}
